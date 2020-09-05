581d1c27f1acaf93ebde37fc281b37b82b52d857
Langmouren <1921489356@qq.com>
2020-09-05 10:47:26 +0800

    浪轩的Git啊

---
+++ src/main/java/com/lang/HelloGit.java
@@ -0,0 +1,14 @@
+package com.lang;
+
+
+/**
+ * @Author huangjie
+ * @Title: HelloGit
+ * @Description 浪某人
+ * @Date 2020/09/05 上午 10:04
+ */
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("浪某人爱你Git");
+    }
+}
